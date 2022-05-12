a26f8c41a269966e3df62595cffb8a60d5aeab04
yoojh5685 <yoojh5685@handong.ac.kr>
2022-05-12 23:55:04 +0900

    함수 정의 추가

--- product.h
+++ product.h
@@ -14,6 +14,6 @@ int createProduct(Product *p); //물품 추가
 void readProduct(Product p); //물품 조회
 int updateProduct(Product *p); //물품 정보 수정
 int deleteProduct(Product *p); //물품 삭제
-int listProduct(Product *s, int index);
+int listProduct(Product *s, int index); //물품의 항목 리스트 띄우기위한 함수
 
 int selectMenu(); //번호
