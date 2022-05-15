de0d52dd43c43988874521336696d1a9ddb5b4ed
yoojh5685 <yoojh5685@handong.ac.kr>
2022-05-16 04:14:01 +0900

    중복선언 안되게

--- product.h
+++ product.h
@@ -2,6 +2,8 @@
 #include <string.h>
 #include <stdlib.h>
 
+#ifndef _Manager_H_
+#define _Manager_H_
 typedef struct{
 	char name[50];
 	int ex_date; // 유통기한
@@ -10,10 +12,12 @@ typedef struct{
 	int stock_num;// 물품 재고 수
 }Product;
 
-int createProduct(Product *p); //물품 추가
+#endif
+
 void readProduct(Product p); //물품 조회
+int listProduct(Product *s, int index); //물품의 항목 리스트 띄우기위한 함수
+int createProduct(Product *p); //물품 추가
 int updateProduct(Product *p); //물품 정보 수정
 int deleteProduct(Product *p); //물품 삭제
-int listProduct(Product *s, int index); //물품의 항목 리스트 띄우기위한 함수
 
 int selectMenu(); //번호
