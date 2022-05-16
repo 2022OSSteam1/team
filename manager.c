8b3a4c31052ecb11dd14db3628cbe16d8d3e5526
sungjin555555 <22100448@handong.ac.kr>
2022-05-16 15:48:22 +0900

    update manager.c

--- manager.c
+++ manager.c
@@ -73,6 +73,7 @@ void searchPrice(Product *p, int index){	// 가격으로 검색
                         readProduct(p[i]);
                         scntt++;
                 }
+		printf("%d개 물품 검색됨!\n", scnt);
         }
         if(scntt==0) printf("\n=> 검색된 데이터 없음!");
                 printf("\n");
@@ -89,9 +90,9 @@ int searchEx(Product p[], int count){
                 if(p[i].ex_date == ex){
 			printf("\n=> %d 번 ", i+1);
                         readProduct(p[i]);
-                        //printf("\n=>해당 물품: %s\n", p[i].name);
                         count_ex++;
                 }
+		printf("%d개 물품 검색됨!\n", count_ex);
         }
         if(count_ex==0) printf("=> 유통기한이 %d인 물품은 존재하지 않습니다.\n", ex);
         return 0;
@@ -107,9 +108,9 @@ int searchRemain(Product p[], int count){
                 if(p[i].stock_num == remain){
 			printf("\n=> %d 번 ", i+1);
                         readProduct(p[i]);
-                //printf("\n=>해당 물품: %s\n", p[i].name);
                 count_remain++;
                 }
+		printf("%d개 물품 검색됨!\n", count_remain);
         }
         if(count_remain == 0) printf("=>재고 개수가 %d인 물품은 존재하지 않습니다.\n", remain);
         return 0;
